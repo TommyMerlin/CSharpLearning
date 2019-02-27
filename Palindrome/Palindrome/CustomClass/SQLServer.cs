a7b1eb1f8072b583bb0965fd310a42892bc950b4
TommyMerlin <786731256@qq.com>
2019-02-27 14:32:03 +0800

    2/27/2

--- Palindrome/Palindrome/CustomClass/SQLServer.cs
+++ Palindrome/Palindrome/CustomClass/SQLServer.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
-namespace Palindrome.CustomClass
+namespace Palindrome
 {
     class SQLServer
     {
+        private static SqlConnection conn = null;
+        private static SqlCommand cmd = null;
+
+        static void Main(string[] args)
+        {
+            string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["connStr"].ConnectionString.ToString();
+            conn = new SqlConnection(conStr);
+            conn.Open();
+            string insert = "insert into node(id,x,y)values(23,23,23)";
+            cmd = new SqlCommand(insert, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine("Data inserted successfully!");
+
+            Console.Read();
+        }
     }
 }
