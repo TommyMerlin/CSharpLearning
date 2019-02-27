7aa27af2e455b2fdb48f8cf8cccb6bd78ae2dba1
TommyMerlin <786731256@qq.com>
2019-02-27 15:52:38 +0800

    2/27/4

---
+++ Palindrome/DAL/UserDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class UserDB
+    {
+        private string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["connStr"].ConnectionString.ToString();
+
+        public void AddUser(Model.UserInfo userInfo)
+        {
+            userInfo.UserName = "huye";
+            userInfo.UserPassword = "123456";
+            Console.WriteLine($"UserName: {userInfo.UserName}\nUserPassword: {userInfo.UserPassword}");
+        }
+    }
+}
---
+++ Palindrome/Model/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="userPassword">用户密码</param>
+        public UserInfo(string userName, string userPassword)
+        {
+            UserName = userName;
+            UserPassword = userPassword;
+        }
+
+        public string UserName { get; set; }         //用户名
+        public string UserPassword { get; set; }     //用户密码
+    }
+}
