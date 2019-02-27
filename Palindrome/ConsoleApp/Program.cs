3e87e4642b6526d9ffaa4b050dfb75b824978169
TommyMerlin <786731256@qq.com>
2019-02-27 18:31:51 +0800

    2/27/8

--- Palindrome/ConsoleApp/Program.cs
+++ Palindrome/ConsoleApp/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    class Enum
+    {
+        /// <summary>
+        /// 连接状态
+        /// </summary>
+        public enum ConnecttionState
+        {
+            Disconnected,
+            Connecting,
+            Connected,
+            Disconnecting
+        }
+
+        public void Operate(ConnecttionState connecttionState)
+        {
+            switch (connecttionState)
+            {
+                case ConnecttionState.Connected:
+                    Console.WriteLine("Connected");
+                    break;
+                case ConnecttionState.Connecting:
+                    Console.WriteLine("Connecting");
+                    break;
+                default:
+                    Console.WriteLine("Hello");
+                    break;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(1<<2);
+
+            Console.Read();
         }
     }
 }
--- Palindrome/Palindrome/CustomClass/Out.cs
+++ Palindrome/Palindrome/CustomClass/Out.cs
@@ -6,35 +6,5 @@ using System.Threading.Tasks;
 
 namespace Palindrome
 {
-    class Out
-    {
-        static void Main(string[] args)
-        {
-            string firstName;
-            string lastName;
-            //string fullName = GetUserInput(out firstName, out lastName);
-            //Console.WriteLine(firstName);
-            //Console.WriteLine(lastName);
-            //Console.WriteLine(fullName);
-
-            string ageText = Console.ReadLine();
-            if(int.TryParse(ageText,out int age))
-            {
-                Console.WriteLine($"Your are {age} years old");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-
-            Console.Read();
-        }
-        
-        static string GetUserInput(out string firstName, out string lastName)
-        {
-            firstName = Console.ReadLine();
-            lastName = Console.ReadLine();
-            return firstName + " " + lastName;
-        }
-    }
+    
 }
