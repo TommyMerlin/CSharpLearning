f1c769ed4baa1dca14eebb9575f26c435067fd2b
TommyMerlin <786731256@qq.com>
2019-03-11 19:38:42 +0800

    3/11/6

--- DataBinding/DataBinding/MainWindow.xaml.cs
+++ DataBinding/DataBinding/MainWindow.xaml.cs
@@ -12,17 +12,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DataBinding
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
+
+        private int _Num;
+        public int Num
+        {
+            get { return _Num; }
+            set
+            {
+                if(_Num != value)
+                {
+                    _Num = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
