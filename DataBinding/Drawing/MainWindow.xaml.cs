24aa9a45b3541902dd60954e35c1ef5972b8519a
TommyMerlin <786731256@qq.com>
2019-03-18 13:02:26 +0800

    3/18/1

--- DataBinding/Drawing/MainWindow.xaml.cs
+++ DataBinding/Drawing/MainWindow.xaml.cs
@@ -33,5 +33,47 @@ namespace Drawing
         {
             frame.Source = new Uri(((TreeViewItem)sender).Tag.ToString(), UriKind.Relative);
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rabtn = (RadioButton)sender;
+            switch (rabtn.Content)
+            {
+                case "OP1":
+                    MessageBox.Show("OP1!");
+                    break;
+                case "OP2":
+                    MessageBox.Show("OP2!");
+                    break;
+                case "OP3":
+                    MessageBox.Show("OP3!");
+                    break;
+                case "OP4":
+                    MessageBox.Show("OP4!");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.FontWeight = FontWeights.Bold;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.FontWeight = FontWeights.Normal;
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            this.FontStyle = FontStyles.Italic;
+        }
+
+        private void CheckBox_Unchecked_1(object sender, RoutedEventArgs e)
+        {
+            this.FontStyle = FontStyles.Normal;
+        }
     }
 }
