96644c0ef01a12fc98f9c5ff86ccbfd3313299d2
TommyMerlin <786731256@qq.com>
2019-04-27 14:49:05 +0800

    4/27/1

---
+++ CommandProgram/CommandProgram/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandProgram
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeCommand();
+        }
+
+        private RoutedCommand clearCmd = new RoutedCommand("Clear", typeof(MainWindow));
+
+        public void InitializeCommand()
+        {
+            this.btn1.Command = this.clearCmd;
+            this.clearCmd.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Alt));
+
+            this.btn1.CommandTarget = this.txtbox1;
+
+            CommandBinding cbd = new CommandBinding();
+            cbd.Command = this.clearCmd;
+            cbd.CanExecute += Cbd_CanExecute;
+            cbd.Executed += Cbd_Executed;
+            this.stackPanel.CommandBindings.Add(cbd);
+        }
+
+        private void Cbd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.txtbox1.Clear();
+            e.Handled = true;
+        }
+
+        private void Cbd_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbox1.Text)) { e.CanExecute = false; }
+            else { e.CanExecute = true; }
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbox1.Text)) { e.CanExecute = false; }
+            else { e.CanExecute = true; }
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string par = e.Parameter.ToString();
+            switch (par)
+            {
+                case "Student":
+                    lbName.Items.Add($"New Student: {txtbox1.Text}");
+                    break;
+                case "Teacher":
+                    lbName.Items.Add($"New Teacher: {txtbox1.Text}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- DependencyProperty/WpfApp1/MainWindow.xaml.cs
+++ DependencyProperty/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,40 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Student> StudentList;
+
         public MainWindow()
         {
             InitializeComponent();
+            StudentList = new ObservableCollection<Student>()
+            {
+                new Student("Tom",12,1001),
+                new Student("Jerry",22,1002),
+                new Student("Amy",23,1003),
+                new Student("Sheldon",32,1004),
+                new Student("Tony",45,1005),
+            };
+            lbox.ItemsSource = StudentList;
+            grid1.ItemsSource = StudentList;
         }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            StudentList.Add(new Student("Hugo", 5, 1006));
+        }
+    }
+
+    public class Student
+    {
+        public Student(string name, int age, int iD)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Age = age;
+            ID = iD;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ID { get; set; }
     }
 }
