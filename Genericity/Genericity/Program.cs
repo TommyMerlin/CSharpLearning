6ef80986f503902b14e3d52ca9486659e2360c36
TommyMerlin <786731256@qq.com>
2019-08-12 18:27:52 +0800

    Genericity#1

--- Genericity/Genericity/Program.cs
+++ Genericity/Genericity/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Genericity
 {
+    public class GenericClass<T>
+    {
+        T[] TArray = new T[10];
+        public int position = 0;
+        public void push(T t)
+        {
+            position++;
+            TArray[position] = t;
+        }
+        public T push()
+        {
+            position--;
+            return TArray[position];
+        }
+        public T[] GetTs() => TArray;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var gClass = new GenericClass<string>();
+            gClass.push("ZJU");
+            gClass.push("YQ");
+            for (int i = 0; i < gClass; i++)
+            {
+                Console.WriteLine(gClass[i]);
+            }
         }
     }
 }
