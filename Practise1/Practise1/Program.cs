3c66ee2dd06a15467ac3748c386492b347902fe6
TommyMerlin <786731256@qq.com>
2019-08-29 17:07:15 +0800

    8/29/1

--- Practise1/Practise1/Program.cs
+++ Practise1/Practise1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,93 +10,92 @@ using iText.Layout.Element;
 
 namespace Practise1
 {
-    public class BusTicketSys
+    class ParentClass
     {
-        public string[,] Matrix { get; set; }
-        
-        public BusTicketSys()
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public ParentClass()
         {
-            Matrix = new string[9, 4];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Matrix[i, j] = "有票";
-                }
-            }
+            Age = 12;
+            Name = "HUYE";
         }
 
-        public void ShowMatrix()
+        public ParentClass(int age, string name)
         {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write("{0,-6}",Matrix[i,j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public void ModifyMatrix(int row,int col)
-        {
-            Matrix[row, col] = "无票";
+            Age = age;
+            Name = name;
         }
     }
 
-    class MyClass : IDisposable
+    class ChildClass : ParentClass
     {
+        public int Height { get; set; }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // 要检测冗余调用
-
-        protected virtual void Dispose(bool disposing)
+        public ChildClass()
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: 释放托管状态(托管对象)。
-                }
-
-                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
-                // TODO: 将大型字段设置为 null。
-
-                disposedValue = true;
-            }
+            Height = 178;
         }
 
-        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
-        // ~MyClass()
-        // {
-        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-        //   Dispose(false);
-        // }
-
-        // 添加此代码以正确实现可处置模式。
-        public void Dispose()
+        public ChildClass(int age,string name)
         {
-            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-            Dispose(true);
-            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
-            // GC.SuppressFinalize(this);
+            Height = 180;
         }
-        #endregion
+    }
 
+    public abstract class BaseClass
+    {
+        public abstract void Print();
+    }
+
+    public class ExtendClass : BaseClass
+    {
+        public virtual void Display() { }
+
+        public override void Print()
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 
     class Program
     {
+        public delegate bool ComparisonHandler(int first, int second);
+
+        public static bool AlphabetGreaterThan(int first, int second)
+        {
+            int comparison;
+            comparison = first.ToString().CompareTo(second.ToString());
+            return comparison > 0;
+        }
+
+        public static void BubbleSort(int[] arr, ComparisonHandler ComparisonMethod)
+        {
+            if (arr == null)
+            {
+                return;
+            }
+            int temp = 0;
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (ComparisonMethod(arr[j-1],arr[j]))
+                    {
+                        temp = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-
-            PdfWriter writer = new PdfWriter("Hello.pdf");
-            PdfDocument pdf = new PdfDocument(writer);
-            Document doc = new Document(pdf);
-            doc.Add(new Paragraph("Hello World!"));
-            doc.Close();
-
-
+            ChildClass c1 = new ChildClass();
+            ChildClass c2 = new ChildClass(13, "ZJU");
+            Console.WriteLine($"{c1.Name}:{c1.Age} {c1.Height}");
+            Console.WriteLine($"{c2.Name}:{c2.Age} {c2.Height}");
 
             Console.Read();
         }
